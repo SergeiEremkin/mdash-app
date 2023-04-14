39e42f8b0423c31b3d5d9d64143ce5a670c6f4a6
Sergei Eremkin <ffr@list.ru>
2023-04-14 21:00:48 +0300

    make migrations and test view, all works

--- manager_base/urls.py
+++ manager_base/urls.py
@@ -14,15 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
+
+
 from django.contrib import admin
 from django.urls import path, include
-
+from manager_base import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('manager_base/', include('manager_base.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
+    path('', views.index, name='home')
+]
